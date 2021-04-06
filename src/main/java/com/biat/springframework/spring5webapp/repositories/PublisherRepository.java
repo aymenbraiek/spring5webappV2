f4147a633cf7c38f87778b0a75a781d324b409fc
aymenbraiek <aymen.braiek@esprit.tn>
2021-04-06 22:47:06 +0100

    adding publisher entity+repository+initializing data for publisher -closes #5

---
+++ src/main/java/com/biat/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.biat.springframework.spring5webapp.repositories;
+
+import com.biat.springframework.spring5webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher,Long> {
+}
