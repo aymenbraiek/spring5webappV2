565424d44111603f316f7d5e7cdadbb4239ccb1b
aymenbraiek <aymen.braiek@esprit.tn>
2021-04-07 22:56:29 +0100

    adding thymeleaf template +releationship Publisher and books -closes #8

--- src/main/java/com/biat/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/com/biat/springframework/spring5webapp/domain/Publisher.java
@@ -1,10 +1,9 @@
 package com.biat.springframework.spring5webapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -16,7 +15,9 @@ public class Publisher {
     private String city;
     private String state;
     private String zip;
-
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
     public Long getId() {
         return id;
     }
@@ -27,6 +28,14 @@ public class Publisher {
 
     public String getName() {
         return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     public void setName(String name) {
